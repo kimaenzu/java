80512f1d15f98ec28d4d66f8ef93603f5127bd99
kimaenzu <117630014+kimaenzu@users.noreply.github.com>
2023-04-13 08:59:33 +0900

    Update Situation.java

--- Situation.java
+++ Situation.java
@@ -7,37 +7,32 @@ public class Situation {
 	private int smog;// 연기농도
 	private int humi;// 습도
 
-	//tmp 필드의 Getter/Setter 선언
+	//필드의 Getter/Setter 선언
+
 	public int getTmp() {
 		return tmp;
 	}
 	public void setTmp(int tmp) {
-		if (tmp < -10 && 120 < tmp) { // 온도가 범위 밖일 때
-			this.tmp = 18; // 초기값으로 지정
-		} else {
-			this.tmp=tmp; //현재 값으로 지정
-		}
+		this.tmp = tmp;
 	}
+
+
 	public int getSmog() {
 		return smog;
 	}
 	public void setSmog(int smog) {
-		if (smog < 0 && 40 < smog) { // 온도가 범위 밖일 때
-			this.smog = 0; // 초기값으로 지정
-		} else {
-			this.smog=smog; //현재 값으로 지정
-		}
+		this.smog = smog;
 	}
+
+
 	public int getHumi() {
 		return humi;
 	}
 	public void setHumi(int humi) {
-		if (humi < -20 && 150 < humi) { // 온도가 범위 밖일 때
-			this.humi = 55; // 초기값으로 지정
-		} else {
-			this.humi=humi; //현재 값으로 지정
-		}
+		this.humi = humi;
 	}
+
+
 	
 	@Override
 	public String toString() {
